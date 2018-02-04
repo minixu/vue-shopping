c844ca0cfaa315c77805c6ed3f94aad64db604ed
minixu <307287103@qq.com>
2018-02-05 00:54:48 +0800

    1

---
+++ app.js
@@ -0,0 +1,24 @@
+var http = require("http")
+var fs = require("fs")
+
+http.createServer(function(req,res){
+    var path = req.url;
+    console.log("path1: "+path)
+    if(path == "/"){
+        path = "/index.html";
+    }
+    sendFile(res,path);
+}).listen(3001)
+
+function sendFile(res,path){
+    var path = process.cwd()+path;
+    fs.readFile(path,function(err,stdout,stderr){
+        if(!err){
+            var data = stdout;
+            var type = path.substr(path.lastIndexOf(".")+1,path.length)
+            res.writeHead(200,{'Content-type':"text/"+type});   //在这里设置文件类型，告诉浏览器解析方式
+            res.write(data);
+        }
+        res.end();
+    })
+}
